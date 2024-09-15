ce6ed04eeba9a02e32488916a3e41481b45b196b
mohamedlemroudi <mlemroudi700@gmail.com>
2024-09-15 16:01:20 -0400

    Templates: about, projects and tasks.

--- myapp/views.py
+++ myapp/views.py
@@ -6,20 +6,20 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+
+def about(request):
+    return render(request, 'about.html')
+
+
 def hello(request, username):
     return HttpResponse("<h1>Hello %s!</h1>" % username)
 
-
-def about(request):
-    return HttpResponse("About")
-
-
 def projects(request):
     projects = list(Project.objects.values())
-    return JsonResponse(projects, safe=False)
+    return render(request, 'projects.html')
 
 
-def tasks(request, title):
-    task = Task.objects.get(title=title)
+def tasks(request):
+    # task = Task.objects.get(title=title)
     # task = get_object_or_404(Task, id=id)
-    return HttpResponse('task: %s' % task.title)
+    return render(request, 'tasks.html')
