e98714b18eed9eb98f11f0d06e858c8b5f8fa938
mohamedlemroudi <mlemroudi700@gmail.com>
2024-09-15 14:55:10 -0400

    Creating a query for the Projects and Tasks classes.

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,14 @@ def hello(request, username):
 
 def about(request):
     return HttpResponse("About")
+
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+
+def tasks(request, id):
+    # task = Task.objects.get(id=id)
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse('task: %s' % task.title)
