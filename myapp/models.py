f4b75b8f26fbede46c546fe61cb8e495480f2131
mohamedlemroudi <mlemroudi700@gmail.com>
2024-09-15 15:09:29 -0400

    Only the name of the projects is visible.

--- myapp/models.py
+++ myapp/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
