10d66e9c961004a1e944b36b40fc563dcf09a767
mohamedlemroudi <mlemroudi700@gmail.com>
2024-09-15 12:00:51 -0400

    Creating the task class

--- myapp/models.py
+++ myapp/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=200)
 
-
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
