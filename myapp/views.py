aefed5b538724b411edaf13a064bcf4487874734
mohamedlemroudi <mlemroudi700@gmail.com>
2024-09-17 21:08:29 -0400

    Enter a name for the URLs.

--- myapp/views.py
+++ myapp/views.py
@@ -45,7 +45,7 @@ def create_task(request):
     else:
         Task.objects.create(title=request.POST['title'], 
         description=request.POST['description'], project_id=2)
-        return redirect('/tasks/')
+        return redirect('tasks')
 
 
 def create_project(request):
@@ -54,8 +54,6 @@ def create_project(request):
             'form': CreateNewProject()
         })
     else:
-        project = Project.objects.create(name=request.POST["name"])
-        return render(request, 'projects/create_project.html', {
-            'form': CreateNewProject()
-        })        
+        Project.objects.create(name=request.POST["name"])
+        redirect('projects')        
         
