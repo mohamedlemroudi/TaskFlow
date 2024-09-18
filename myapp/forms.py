5226828628f74b377070e2e332763e7b3d471755
mohamedlemroudi <mlemroudi700@gmail.com>
2024-09-17 22:25:30 -0400

    Restructuring the forms.py file.

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,10 +1,30 @@
 from django import forms
 
 class CreateNewTask(forms.Form):
-    title = forms.CharField(label="Titulo de la tarea:", max_length=200, widget=forms.TextInput(attrs={'class':'input'}))
+    """
+    Form for creating a new task.
+    - title: Text field for the task title.
+    - description: Text area for the task description.
+    """
+    title = forms.CharField(
+        label="Task Title:",
+        max_length=200,
+        widget=forms.TextInput(attrs={'class': 'input'})
+    )
    
-    description = forms.CharField(label="Descripcion de la tarea: ",widget=forms.Textarea(attrs={'class':'input'}))
+    description = forms.CharField(
+        label="Task Description:",
+        widget=forms.Textarea(attrs={'class': 'input'})
+    )
 
 
 class CreateNewProject(forms.Form):
-    name = forms.CharField(label="Nombre del Proyecto:", max_length=200, widget=forms.TextInput(attrs={'class':'input'}))
+    """
+    Form for creating a new project.
+    - name: Text field for the project name.
+    """
+    name = forms.CharField(
+        label="Project Name:",
+        max_length=200,
+        widget=forms.TextInput(attrs={'class': 'input'})
+    )
