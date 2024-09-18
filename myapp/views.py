96c2b854d828c3853170191b499aa40e2b857b99
mohamedlemroudi <mlemroudi700@gmail.com>
2024-09-17 22:01:00 -0400

    Improving CSS styles.

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Project, Task
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CreateNewTask, CreateNewProject
 
 # Create your views here.
@@ -55,5 +55,13 @@ def create_project(request):
         })
     else:
         Project.objects.create(name=request.POST["name"])
-        redirect('projects')        
+        return redirect('projects')        
         
+
+def project_detail(request, id):
+    project = get_object_or_404(Project, id=id)
+    tasks = Task.objects.filter(project_id=id)
+    return render(request, 'projects/detail.html', {
+        'project':project,
+        'tasks': tasks
+    })
